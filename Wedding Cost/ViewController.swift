1c046dc9e840ff6e76c82d820d2346b5bf4ab06e
Adstyle <mohammadadlan91@gmail.com>
2014-10-31 18:43:36 +0800

    Initial Commit

---
+++ Wedding Cost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wedding Cost
+//
+//  Created by Mohammad Adlan on 10/31/14.
+//  Copyright (c) 2014 alan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
