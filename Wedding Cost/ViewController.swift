2c4ec7adbf9c5371a95b1d69547e3ae57601a6a0
Adstyle <mohammadadlan91@gmail.com>
2014-10-31 19:19:26 +0800

    wedding & engagement cost calculator

--- Wedding Cost/ViewController.swift
+++ Wedding Cost/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var engageRing: UITextField!
+    @IBOutlet weak var firstItem: UITextField!
+    @IBOutlet weak var secondItem: UITextField!
+    @IBOutlet weak var thirdItem: UITextField!
+    @IBOutlet weak var fourthItem: UITextField!
+    @IBOutlet weak var fifthItem: UITextField!
+    @IBOutlet weak var costLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func calcButton(sender: UIButton) {
+        
+        let engageRingValue = Double((engageRing.text as NSString).doubleValue)
+        let firstItemValue = Double((firstItem.text as NSString).doubleValue)
+        let secondItemValue = Double((secondItem.text as NSString).doubleValue)
+        let thirdItemValue = Double((thirdItem.text as NSString).doubleValue)
+        let fourthItemValue = Double((fourthItem.text as NSString).doubleValue)
+        let fifthItemValue = Double((fifthItem.text as NSString).doubleValue)
+        
+        costLabel.hidden = false
+        
+        costLabel.text = "\(engageRingValue + firstItemValue + secondItemValue + thirdItemValue + fourthItemValue + fifthItemValue)"
+        
+    }
 
 }
-
